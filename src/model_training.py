d67e9a6414f59ab2824667dc203a57dfd2e19ece
Stefano2599 <E202521620@upc.edu.pe>
2026-06-23 22:00:43 -0500

    Add model training script with selected models

---
+++ src/model_training.py
@@ -0,0 +1,6 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neural_network import MLPRegressor
+
+print("Modelos seleccionados:")
+print("- Random Forest")
+print("- Red Neuronal Artificial")
